11059c34f5d72049d54b6383d0e033a8963e8a7c
ElPumpo <samo_kol@hotmail.com>
2016-09-27 21:40:26 +0200

    finally solved git conflict

--- Win10Clean/About.Designer.vb
+++ Win10Clean/About.Designer.vb
@@ -22,7 +22,7 @@ Partial Class About
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-<<<<<<< HEAD
+
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
         Me.CloseBtn = New System.Windows.Forms.Button()
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
@@ -61,48 +61,6 @@ Partial Class About
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "About"
         Me.ResumeLayout(False)
-
-=======
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
-        Me.CloseBtn = New System.Windows.Forms.Button()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.SuspendLayout()
-        '
-        'CloseBtn
-        '
-        Me.CloseBtn.Location = New System.Drawing.Point(292, 240)
-        Me.CloseBtn.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.CloseBtn.Name = "CloseBtn"
-        Me.CloseBtn.Size = New System.Drawing.Size(58, 19)
-        Me.CloseBtn.TabIndex = 1
-        Me.CloseBtn.Text = "Go back"
-        Me.CloseBtn.UseVisualStyleBackColor = True
-        '
-        'RichTextBox1
-        '
-        Me.RichTextBox1.Location = New System.Drawing.Point(9, 10)
-        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.ReadOnly = True
-        Me.RichTextBox1.Size = New System.Drawing.Size(343, 227)
-        Me.RichTextBox1.TabIndex = 2
-        Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
-        '
-        'About
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(360, 269)
-        Me.Controls.Add(Me.RichTextBox1)
-        Me.Controls.Add(Me.CloseBtn)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.Name = "About"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "About"
-        Me.ResumeLayout(False)
-
->>>>>>> 433da21d71ca600d63d67a18d5bea5bc47e3de2f
     End Sub
     Friend WithEvents CloseBtn As Button
     Friend WithEvents RichTextBox1 As RichTextBox
