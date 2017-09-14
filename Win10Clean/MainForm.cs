7fd6c2e4ec00e6ff6eab676153584b94f8b5df7e
dead moon <deadtears@outlook.com>
2017-09-14 22:39:58 +0300

    added license info

--- Win10Clean/MainForm.cs
+++ Win10Clean/MainForm.cs
@@ -12,6 +12,24 @@ using System.Net;
 using System.IO;
 using System.Diagnostics;
 
+/*
+ * Win10Clean - Cleanup your Windows 10 environment
+ * Copyright (C) 2017 Hawaii_Beach & deadmoon
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the license, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 namespace Win10Clean
 {
     public partial class MainForm : Form
